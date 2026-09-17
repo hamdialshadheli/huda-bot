f8a857a3ca44ae2c7a4bb52e499e3484935341f4
Hamdi qahtan AL_Shadheli <150554582+hamdialshadheli@users.noreply.github.com>
2026-09-17 18:40:23 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
     Application,
     CommandHandler,
